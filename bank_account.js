7664cce491de60a67cbb35474df07d8702443054
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-09-26 23:01:29 +0700

    [REFACTOR] added classes and class method for tambahSaldo()

--- bank_account.js
+++ bank_account.js
@@ -1,15 +1,58 @@
-let saldo = 0
+class BankAccount {
+  constructor() {
+    this.saldo = 0;
+  }
 
-tambahSaldo = () => {
-    saldo += parseInt(prompt("Masukan jumlah saldo yang akan ditambahkan"))
-    tampilkanSaldo()
+  cekSaldo() {
+    return this.saldo;
+  }
+
+  tambahSaldo() {
+    let value = prompt('Masukan jumlah saldo yang akan ditambahkan');
+
+    if (!isNumber(value)) {
+      alert('You are not entering a number!');
+      return;
+    }
+
+    this.saldo += parseInt(value);
+  }
 }
+
+const bankAccount = new BankAccount();
+const tambahSaldoButton = document.getElementById('tambahSaldo');
+const kurangiSaldoButton = document.getElementById('kurangiSaldo');
+
+tambahSaldoButton.addEventListener('click', () => {
+  bankAccount.tambahSaldo();
+  alert(`saldo kamu saat ini : ${bankAccount.cekSaldo()}`);
+});
+
+isNumber = (input) => {
+  if (isNaN(Number(input))) {
+    return false;
+  }
+
+  return true;
+};
 
 kurangiSaldo = () => {
-    saldo -= parseInt(prompt("Masukan jumlah saldo yang akan ditambahkan"))
-    tampilkanSaldo()
-}
+  let value = prompt('Masukan jumlah saldo yang akan dikurangi');
+
+  if (isNaN(Number(value))) {
+    alert('You are not entering a number!');
+    return;
+  }
+
+  if (parseInt(value) > saldo) {
+    alert('Error, Low balance');
+    return;
+  }
+
+  saldo -= parseInt(value);
+  tampilkanSaldo();
+};
 
 tampilkanSaldo = () => {
-    alert(`Saldo terakhir : ${saldo}`)
-}
+  alert(`Saldo terakhir : ${saldo}`);
+};
